dff4f830ddd508cfee4d97d3043d585069e6394e
JohnPhilip80 <rushtophilip@gmail.com>
2026-05-24 23:48:15 -0700

    Refactor product initialization to use a list
    Replaced individual product declarations with a list to manage products more efficiently.

--- Demo.java
+++ Demo.java
@@ -1,36 +1,30 @@
 package edu.mce.john.ims;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import edu.mce.john.ims.model.DigitalProduct;
 import edu.mce.john.ims.model.PhysicalProduct;
 import edu.mce.john.ims.model.Product;
+import edu.mce.john.ims.service.Order;
 
 public class Demo {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Inventory Management System");
 		
-		Product p1 = new PhysicalProduct("P-101-2026","Lenova Laptop",39800.00,2.0);
-		Product p2 = new PhysicalProduct("P-102-2026","Samsung Phone",63700.00,0.7);
-		Product p3 = new PhysicalProduct("P-103-2026","Office Rolling Chair",20600.00,20.0);
-		Product p4 = new PhysicalProduct("P-104-2026","Office Desk",30300.00,35.0);
-		Product p5 = new DigitalProduct("D-105-2026","EBook Java Learning",3600.00);
-		Product p6 = new DigitalProduct("D-106-2026","Advanced Java Learning Video",7300.00);
+		List<Product> productList = new ArrayList<Product>();
 		
-		System.out.printf("%-15s%-30s%-15s%-15s%-30s\n",
-				"Id",
-				"Name",
-				"Base Price",
-				"Final Price",
-				"Price Info");
+		productList.add(new PhysicalProduct("P-101-2026","Lenova Laptop",39800.00,2.0));
+		productList.add(new PhysicalProduct("P-102-2026","Samsung Phone",63700.00,0.7));
+		productList.add(new PhysicalProduct("P-103-2026","Office Rolling Chair",20600.00,20.0));
+		productList.add(new PhysicalProduct("P-104-2026","Office Desk",30300.00,35.0));
+		productList.add(new DigitalProduct("D-105-2026","EBook Java Learning",3600.00));
+		productList.add(new DigitalProduct("D-106-2026","Advanced Java Learning Video",7300.00));
 		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-		System.out.println(p4);
-		System.out.println(p5);
-		System.out.println(p6);
-
+		Order myOrder = new Order("ORD-2026-001",
+				"John Philip",
+				productList);
+		myOrder.generateInvoice();
 	}
-
 }
-
